3fe4e7c1b7e6297c6ae74ba8db8fb9201d94bff8
Arzeezar <72872978+Arzeezar@users.noreply.github.com>
2024-02-27 18:45:03 +0700

    update inpainting model

--- partial_conv/generator.py
+++ partial_conv/generator.py
@@ -1,8 +1,15 @@
 from tensorflow import keras
 import tensorflow as tf
+
+from keras.models import Model
+from keras.models import load_model
+from keras.optimizers import Adam
+from keras.layers import Input, Conv2D, UpSampling2D, Dropout, LeakyReLU, BatchNormalization, Activation, Lambda, Concatenate
+from keras import backend as K
 ## utils is present in the cloned repo. Visit repo for the implementation of PConv2D.
 # from deepimageinpainting.utils.pconv_layer import PConv2D
-from .deepimageinpainting.utils.pconv_layer import PConv2D
+# from .deepimageinpainting.utils.pconv_layer import PConv2D
+from .partial_convolution_keras.libs.pconv_layer import PConv2D
 
 ## For more information into formulation: https://www.youtube.com/watch?v=AZr64OxshLo
 ## Metric
@@ -16,61 +23,60 @@ class InpaintingModel:
   '''
   Build UNET like model for image inpaining task.
   '''
-  def prepare_model(self, input_size=(32,32,3), output_type = 'inpaint'):
-    initializer = tf.random_normal_initializer(0., 0.02)
-    input_image = keras.layers.Input(input_size)
-    input_mask = keras.layers.Input(input_size, name='encoder_input')
+  def build_pconv_unet(self, input_size=(32,32,3), train_bn=True):
+    # INPUTS
+        inputs_img = Input(input_size, name='inputs_img')
+        inputs_mask = Input(input_size, name='inputs_mask')
+        
+        # ENCODER
+        def encoder_layer(img_in, mask_in, filters, kernel_size, bn=True):
+            conv, mask = PConv2D(filters, kernel_size, strides=2, padding='same')([img_in, mask_in])
+            if bn:
+                conv = BatchNormalization(name='EncBN'+str(encoder_layer.counter))(conv, training=train_bn)
+            conv = Activation('relu')(conv)
+            encoder_layer.counter += 1
+            return conv, mask
+        encoder_layer.counter = 0
+        
+        e_conv1, e_mask1 = encoder_layer(inputs_img, inputs_mask, 64, 7, bn=False)
+        e_conv2, e_mask2 = encoder_layer(e_conv1, e_mask1, 128, 5)
+        e_conv3, e_mask3 = encoder_layer(e_conv2, e_mask2, 256, 5)
+        e_conv4, e_mask4 = encoder_layer(e_conv3, e_mask3, 512, 3)
+        e_conv5, e_mask5 = encoder_layer(e_conv4, e_mask4, 512, 3)
+        e_conv6, e_mask6 = encoder_layer(e_conv5, e_mask5, 512, 3)
+        e_conv7, e_mask7 = encoder_layer(e_conv6, e_mask6, 512, 3)
+        e_conv8, e_mask8 = encoder_layer(e_conv7, e_mask7, 512, 3)
+        
+        # DECODER
+        def decoder_layer(img_in, mask_in, e_conv, e_mask, filters, kernel_size, bn=True):
+            up_img = UpSampling2D(size=(2,2))(img_in)
+            up_mask = UpSampling2D(size=(2,2))(mask_in)
+            concat_img = Concatenate(axis=3)([e_conv,up_img])
+            concat_mask = Concatenate(axis=3)([e_mask,up_mask])
+            conv, mask = PConv2D(filters, kernel_size, padding='same')([concat_img, concat_mask])
+            if bn:
+                conv = BatchNormalization()(conv)
+            conv = LeakyReLU(alpha=0.2)(conv)
+            return conv, mask
+            
+        d_conv9, d_mask9 = decoder_layer(e_conv8, e_mask8, e_conv7, e_mask7, 512, 3)
+        d_conv10, d_mask10 = decoder_layer(d_conv9, d_mask9, e_conv6, e_mask6, 512, 3)
+        d_conv11, d_mask11 = decoder_layer(d_conv10, d_mask10, e_conv5, e_mask5, 512, 3)
+        d_conv12, d_mask12 = decoder_layer(d_conv11, d_mask11, e_conv4, e_mask4, 512, 3)
+        d_conv13, d_mask13 = decoder_layer(d_conv12, d_mask12, e_conv3, e_mask3, 256, 3)
+        d_conv14, d_mask14 = decoder_layer(d_conv13, d_mask13, e_conv2, e_mask2, 128, 3)
+        d_conv15, d_mask15 = decoder_layer(d_conv14, d_mask14, e_conv1, e_mask1, 64, 3)
+        d_conv16, d_mask16 = decoder_layer(d_conv15, d_mask15, inputs_img, inputs_mask, 3, 3, bn=False)
+        outputs = Conv2D(3, 1, activation = 'sigmoid', name='outputs_img')(d_conv16)
+        
+        # Setup the model inputs / outputs
+        # model = Model(inputs=[inputs_img, inputs_mask], outputs=[outputs])
+        # return keras.models.Model(inputs=[input_image, input_mask], outputs=[outputs])
 
-    conv1, mask1, conv2, mask2 = self.__encoder_layer(32, input_image, input_mask, ['conv1', 'conv2'])
-    conv3, mask3, conv4, mask4 = self.__encoder_layer(64, conv2, mask2, ['conv3', 'conv4'])
-    conv5, mask5, conv6, mask6 = self.__encoder_layer(128, conv4, mask4, ['conv5', 'conv6'])
-    conv7, mask7, conv8, mask8 = self.__encoder_layer(256, conv6, mask6, ['conv7', 'encoder_output'])
-
-    conv9, mask9, conv10, mask10 = self.__decoder_layer(256, 128, conv8, mask8, conv7, mask7, ['conv9', 'conv10'])
-    conv11, mask11, conv12, mask12 = self.__decoder_layer(128, 64, conv10, mask10, conv5, mask5, ['conv11', 'conv12'])
-    conv13, mask13, conv14, mask14 = self.__decoder_layer(64, 32, conv12, mask12, conv3, mask3, ['conv13', 'conv14'])
-    conv15, mask15, conv16, mask16 = self.__decoder_layer(32, 3, conv14, mask14, conv1, mask1, ['conv15', 'decoder_output'])
-
-    outputs = keras.layers.Conv2D(3, (3, 3), activation='sigmoid', padding='same')(conv16)
-
-    # output
-    if(output_type == 'inpaint'):
-        return keras.models.Model(inputs=[input_image, input_mask], outputs=[outputs])
-    elif(output_type == 'v_mask'):
-    # show feature map (remove mask process)
-        return keras.models.Model(inputs=[input_image, input_mask],
-                                outputs=[outputs,
-                                        mask1,mask2,mask3,mask4,mask5,mask6,mask7,mask8,
-                                        mask9,mask10,mask11,mask12,mask13,mask14,mask15,mask16]) # for check mask
-    elif(output_type == 'v_inpaint'):
-    # show feature map (inpaint mask process)
-        return keras.models.Model(inputs=[input_image, input_mask],
-                                outputs=[outputs,
-                                        conv1,conv2,conv3,conv4,conv5,conv6,conv7,conv8,
-                                        conv9,conv10,conv11,conv12,conv13,conv14,conv15,conv16]) # for check mask
-
-  def __encoder_layer(self, filters, in_layer, in_mask, names):
-    conv1, mask1 = PConv2D(32, (3,3), strides=1, padding='same', name=names[0])([in_layer, in_mask])
-    conv1 = keras.activations.relu(conv1)
-
-    conv2, mask2 = PConv2D(32, (3,3), strides=2, padding='same', name=names[1])([conv1, mask1])
-    # conv2 = keras.layers.BatchNormalization()(conv2, training=True)
-    conv2 = keras.activations.relu(conv2)
-
-    return conv1, mask1, conv2, mask2
-
-  def __decoder_layer(self, filter1, filter2, in_img, in_mask, share_img, share_mask, names):
-    up_img = keras.layers.UpSampling2D(size=(2,2))(in_img)
-    up_mask = keras.layers.UpSampling2D(size=(2,2))(in_mask)
-    concat_img = keras.layers.Concatenate(axis=3)([share_img, up_img])
-    concat_mask = keras.layers.Concatenate(axis=3)([share_mask, up_mask])
-
-    conv1, mask1 = PConv2D(filter1, (3,3), padding='same', name=names[0])([concat_img, concat_mask])
-    conv1 = keras.activations.relu(conv1)
-
-    conv2, mask2 = PConv2D(filter2, (3,3), padding='same', name=names[1])([conv1, mask1])
-    # conv2 = keras.layers.BatchNormalization()(conv2)
-    conv2 = keras.activations.relu(conv2)
-
-    return conv1, mask1, conv2, mask2
+        return Model(inputs=[inputs_img, inputs_mask], outputs=[outputs])
+  
+#   def load_model(self,train_bn):
+#     # Create UNet-like model
+#     self.model, inputs_mask = self.build_pconv_unet(train_bn)
+#     # self.compile_pconv_unet(self.model, inputs_mask, lr)
   
